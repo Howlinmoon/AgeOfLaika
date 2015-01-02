d4a72ce2c9a99733d99fdd37368b889fde454009
Jim Veneskey <github@strifecrafter.com>
2015-01-02 12:13:42 -0500

    Initial Commit

---
+++ AgeOfLaika/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AgeOfLaika
+//
+//  Created by jim Veneskey on 1/2/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
